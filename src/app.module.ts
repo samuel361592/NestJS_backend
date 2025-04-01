4a1e4f47d222c7bdddf2c7dd3eb939ff553b547e
samuel361592 <samuel361592@gmail.com>
2025-04-01 23:51:26 +0800

    初步完成 backend 架構與串接mySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { User } from './user/user.entity';
 import { AuthModule } from './auth/auth.module';
 
+
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // 讀取 .env
+
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '3306', 10),
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_DATABASE || '',
+      entities: [User],
+      synchronize: true,
+  }),
+    AuthModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { User } from '../user/user.entity';  // 記得有 user.entity
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([User]),
     JwtModule.register({
-      secret: 'my-secret-key', // 可改成放在環境變數
+      secret: 'my-secret-key', // 建議改用 process.env
       signOptions: { expiresIn: '1h' },
     }),
   ],
